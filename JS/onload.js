0ac55183442d1d27d98c5a3b23f6bafe9b8e0be6
1nastya1 <tanastasia149@gmail.com>
2020-03-07 21:41:28 +0300

    HW2

--- JS/onload.js
+++ JS/onload.js
@@ -4,72 +4,6 @@ window.onload = function(){
 	getOrdInfo(Orders[0].id);
 	activeOrder(Orders[0].id);
 }
-
-function getOrdInfo(id, key){
-	let bufferOrders = Orders.concat();
-	$(".buffer-line-tr").html(" ");
-	bufferOrders.forEach((item, i) =>{
-		if(item.id==id){
-			$(".number-of-order").text("Order " +item.id);
-			$(".custumer").text("Custumer " +item.OrderInfo.customer);
-			$(".ordered").text("Ordered " +item.OrderInfo.createdAt);
-			$(".shipped").text("Shipped " +item.OrderInfo.shippedAt);
-
-			$(".shipped-name").text(item.ShipTo.name);
-      		$(".shiped-address").text(item.ShipTo.Address);
-      		$(".zip").text(item.ShipTo.ZIP);
-      		$(".region").text(item.ShipTo.Region);
-      		$(".country").text(item.ShipTo.Country);
-
-      		$(".name-surname").text(item.CustomerInfo.firstName+" "+item.CustomerInfo.lastName);
-      		$(".custumer-address").text(item.CustomerInfo.address);
-      		$(".phone").text(item.CustomerInfo.phone);
-      		$(".email").text(item.CustomerInfo.email);
-
-      		var total=0;
-      		var t=0;
-      		item.products.forEach((item_prod, i)=>{
-      			total+=parseInt(item_prod.price) * parseInt(item_prod.quantity);
-
-      			if(item_prod.coincidence!=0 && key=="searchRes")
-      			{
-      				t++;
-      				$('<div>',{
-      					class: "line-tr",
-      				}).html(
-      				'<div> <span class="bold naming">'+item_prod.name+'</span> <span class="little number">'+item_prod.id+'</span></div>'+
-		            '<div class="hidder">Unit Price:    </div>'+ '<div> <span><span class="price">'+item_prod.price+'</span><span class="little wallet-table"> '+item_prod.currency+'</span></span></div>'+
-		            '<div class="hidder">Quantity:   </div>'+ 
-		            '<div><span><span class="little">'+item_prod.quantity+'</span></span></div>'+
-		            '<div class="hidder">Total:   </div>'+
-		            '<div class="bold"><span> <span class="price">'+item_prod.totalPrice+'</span><span class="little wallet-table">EUR</span></span></div>'
-		            ).appendTo(".buffer-line-tr")
-      			}
-      			else if(key != "searchRes"){
-      				t++;
-      				$('<div>',{
-      					class: "line-tr",
-      				}).html(
-      				'<div> <span class="bold naming">'+item_prod.name+'</span> <span class="little number">'+item_prod.id+'</span></div>'+
-		            '<div class="hidder">Unit Price:    </div>'+ '<div> <span><span class="price">'+item_prod.price+'</span><span class="little wallet-table">'+item_prod.currency+'</span></span></div>'+
-		            '<div class="hidder">Quantity:   </div>'+ '<div><span><span class="little">'+item_prod.quantity+'</span></span></div>'+
-		            '<div class="hidder">Total:   </div>'+
-		            '<div class="bold"><span> <span class="price">'+item_prod.totalPrice+'</span><span class="little wallet-table">EUR</span></span></div>'
-		            ).appendTo(".buffer-line-tr")
-      			}
-      			$(".lineItem").text("Line Items("+(t)+")");
-      		});
-
-      		$("[data-label=Product]").attr("onClick","sort("+id+",'product')");
-      		$("[data-label=UnitPrice]").attr("onClick","sort("+id+",'price')");
-      		$("[data-label=Quantity]").attr("onClick","sort("+id+",'quantity')");
-     			$("[data-label=Total]").attr("onClick","sort("+id+",'total')");
-     			$("#jsOrderRefresher").attr("onClick","getOrdInfo("+id+")");
-      		$(".line-th").attr("idOrd",id); //хранение id ордера в произвольном аттрибуте
-      		$(".eu").text(total);
-      	}
-    });
-}  
 function add_orders(flag){
 	let bufferOrders = Orders.slice();
 	$(".orders").html(" ");
@@ -150,3 +84,71 @@ function add_orders(flag){
        .appendTo(".orders");
 	});
 }    
+
+
+
+function getOrdInfo(id, key){
+	let bufferOrders = Orders.concat();
+	$(".buffer-line-tr").html(" ");
+	bufferOrders.forEach((item, i) =>{
+		if(item.id==id){
+			$(".number-of-order").text("Order " +item.id);
+			$(".custumer").text("Custumer " +item.OrderInfo.customer);
+			$(".ordered").text("Ordered " +item.OrderInfo.createdAt);
+			$(".shipped").text("Shipped " +item.OrderInfo.shippedAt);
+
+			$(".shipped-name").text(item.ShipTo.name);
+      		$(".shiped-address").text(item.ShipTo.Address);
+      		$(".zip").text(item.ShipTo.ZIP);
+      		$(".region").text(item.ShipTo.Region);
+      		$(".country").text(item.ShipTo.Country);
+
+      		$(".name-surname").text(item.CustomerInfo.firstName+" "+item.CustomerInfo.lastName);
+      		$(".custumer-address").text(item.CustomerInfo.address);
+      		$(".phone").text(item.CustomerInfo.phone);
+      		$(".email").text(item.CustomerInfo.email);
+
+      		var total=0;
+      		var t=0;
+      		item.products.forEach((item_prod, i)=>{
+      			total+=parseInt(item_prod.price) * parseInt(item_prod.quantity);
+
+      			if(item_prod.coincidence!=0 && key=="searchRes")
+      			{
+      				t++;
+      				$('<div>',{
+      					class: "line-tr",
+      				}).html(
+      				'<div> <span class="bold naming">'+item_prod.name+'</span> <span class="little number">'+item_prod.id+'</span></div>'+
+		            '<div class="hidder">Unit Price:    </div>'+ '<div> <span><span class="price">'+item_prod.price+'</span><span class="little wallet-table"> '+item_prod.currency+'</span></span></div>'+
+		            '<div class="hidder">Quantity:   </div>'+ 
+		            '<div><span><span class="little">'+item_prod.quantity+'</span></span></div>'+
+		            '<div class="hidder">Total:   </div>'+
+		            '<div class="bold"><span> <span class="price">'+item_prod.totalPrice+'</span><span class="little wallet-table">EUR</span></span></div>'
+		            ).appendTo(".buffer-line-tr")
+      			}
+      			else if(key != "searchRes"){
+      				t++;
+      				$('<div>',{
+      					class: "line-tr",
+      				}).html(
+      				'<div> <span class="bold naming">'+item_prod.name+'</span> <span class="little number">'+item_prod.id+'</span></div>'+
+		            '<div class="hidder">Unit Price:    </div>'+ '<div> <span><span class="price">'+item_prod.price+'</span><span class="little wallet-table">'+item_prod.currency+'</span></span></div>'+
+		            '<div class="hidder">Quantity:   </div>'+ '<div><span><span class="little">'+item_prod.quantity+'</span></span></div>'+
+		            '<div class="hidder">Total:   </div>'+
+		            '<div class="bold"><span> <span class="price">'+item_prod.totalPrice+'</span><span class="little wallet-table">EUR</span></span></div>'
+		            ).appendTo(".buffer-line-tr")
+      			}
+      			$(".lineItem").text("Line Items("+(t)+")");
+      		});
+
+      		$("[data-label=Product]").attr("onClick","sort("+id+",'product')");
+      		$("[data-label=UnitPrice]").attr("onClick","sort("+id+",'price')");
+      		$("[data-label=Quantity]").attr("onClick","sort("+id+",'quantity')");
+     			$("[data-label=Total]").attr("onClick","sort("+id+",'total')");
+     			$("#jsOrderRefresher").attr("onClick","getOrdInfo("+id+")");
+      		$(".line-th").attr("idOrd",id); //хранение id ордера в произвольном аттрибуте
+      		$(".eu").text(total);
+      	}
+    });
+}  
